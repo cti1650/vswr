f128df01f37fb630a73e6b9fedbdc6dce6ae99fb
cti1650 <cti1650@gmail.com>
2021-07-23 01:27:22 +0900

    darkモード 無効化

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,4 @@
-import Document, { Head, Main, NextScript } from "next/document";
+import Document, { Head, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -8,11 +8,11 @@ export default class MyDocument extends Document {
 
   render() {
     return (
-      <html lang="ja">
+      <html lang='ja'>
         <Head>
           <style>{`body { margin: 0 } /* custom!*/`}</style>
         </Head>
-        <body className="custom_class w-full p-2 container mx-auto dark:bg-black bg-gray-200">
+        <body className='custom_class w-full p-2 container mx-auto bg-gray-200'>
           <Main />
           <NextScript />
         </body>
