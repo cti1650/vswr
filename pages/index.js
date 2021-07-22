3799ebc9b221579ca9c62ab8916f87bb4b3c0e1b
cti1650 <cti1650@gmail.com>
2021-07-22 23:58:37 +0900

    更新

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,74 @@
-import Link from 'next/link'
-import Head from 'next/head'
-import { Slider } from '../src/components/Slider';
+import Link from 'next/link';
+import Head from 'next/head';
+import { Slider } from '../src/components/slider/Slider';
+import { useState, useEffect, useMemo } from 'react';
+import { useRouter } from 'next/router';
+import cc from 'classcat';
 
 export default function IndexPage() {
+  const [tx, setTx] = useState(0);
+  const [rx, setRx] = useState(0);
+  const [ans, setAns] = useState(0);
+  const router = useRouter();
+  const anser = useMemo(() => {
+    return (
+      Math.round(((1 + Math.sqrt(rx / tx)) / (1 - Math.sqrt(rx / tx))) * 100) /
+      100
+    );
+  }, [tx, rx]);
   return (
-    <div>
+    <div className='continer'>
       <Head>
-        <link rel="apple-touch-icon" href="VS.png" />
-        <meta name="apple-mobile-web-app-title" content="VSWR" />
-        <link rel='icon' href='/VS.png' />
+        <link rel='apple-touch-icon' href='VS.png' />
+        <meta name='apple-mobile-web-app-capable' content='yes' />
+        <meta name='apple-mobile-web-app-status-bar-style' content='default' />
+        <meta name='apple-mobile-web-app-title' content='VSWR' />
         <title>ＶＳＷＲ計算ツール</title>
+        <meta name='viewport' content='user-scalable=no' />
+        <meta
+          name='viewport'
+          content='width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0'
+        />
+        <link rel='icon' href='/VS.png' />
       </Head>
-      <Slider />
-      <Slider />
-      <label for="slider">反射波(W):</label>
-      <input type="range" name="slider" id="slider2" value="0" min="0" max="4" step="0.1" data-highlight="true" />
-      <div id="val" className="text-4xl"></div>
+      <div className='text-4xl text-center'>VSWR</div>
+      <div className='w-full pt-8'>
+        <Slider
+          def={tx}
+          label='進行波(W)'
+          onChange={(val) => {
+            setTx(val);
+          }}
+        />
+        <Slider
+          def={rx}
+          label='反射波(W)'
+          max={4}
+          step={0.1}
+          onChange={(val) => {
+            setRx(val);
+          }}
+        />
+      </div>
+      <div
+        id='val'
+        className={cc([
+          'text-6xl text-center',
+          {
+            'text-yellow-600': anser < 3 && anser >= 1.5,
+            'text-red-700': anser >= 3,
+            'text-blue-700': anser < 1.5 && anser > 0,
+          },
+        ])}
+      >
+        {anser}
+      </div>
+      <div className='w-full text-center pt-10'>
+        <img
+          className='mx-auto'
+          src={`https://api.qrserver.com/v1/create-qr-code/?data=${router.pathname}&size=100x100`}
+        />
+      </div>
     </div>
-  )
+  );
 }
--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,8 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useEffect } from 'react';
+import { useCallback } from 'react';
 
 export const Slider = (props) => {
-  return (<>
-    <label for="slider">進行波(W):</label>
-    <input type="range" name="slider" id="slider" value="0" min="0" max="30" step="0.5" data-highlight="true" />
-  </>);
+  const {
+    def = 0,
+    label = '進行波(W)',
+    min = 0,
+    max = 30,
+    step = 0.5,
+    onChange = () => {},
+  } = props;
+  const [val, setVal] = useState(def);
+  const handleSliderChange = useCallback((e) => {
+    setVal(e.target.value);
+  }, []);
+  useEffect(() => {
+    onChange(val);
+  }, [val]);
+  return (
+    <>
+      <div className='w-full'>
+        <label for='slider'>{label}:</label>
+        <input
+          type='range'
+          className='w-full h-10'
+          defaultValue={val}
+          onChange={handleSliderChange}
+          min={min}
+          max={max}
+          step={step}
+          data-highlight='true'
+        />
+        <div className='w-full text-2xl text-right'>{val}</div>
+      </div>
+    </>
+  );
 };
