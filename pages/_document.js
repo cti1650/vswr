c32313df45502b1b2257fa5352123182a76a5ab9
cti1650 <cti1650@gmail.com>
2021-07-24 16:50:43 +0900

    🔨 lighthouse PWA指摘箇所修正

--- pages/_document.js
+++ pages/_document.js
@@ -13,6 +13,7 @@ export default class MyDocument extends Document {
           <style>{`body { margin: 0 } /* custom!*/`}</style>
           <link rel='manifest' href='/manifest.webmanifest' />
           <link rel='apple-touch-icon' href='VSWR.png' />
+          <meta name='theme-color' content='#9bd4e9' />
         </Head>
         <body className='custom_class w-full bg-gray-200'>
           <Main />
