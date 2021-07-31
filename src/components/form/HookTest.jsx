f12bbe611194f1f36f43dc8cbf13ce2594c9d7ad
cti1650 <cti1650@gmail.com>
2021-07-31 09:36:07 +0900

    🎉 カスタムフックの検証

--- src/components/form/HookTest.jsx
+++ src/components/form/HookTest.jsx
@@ -1,12 +1,16 @@
-import { useLocalStorage } from '../src/hooks/useLocalStorage';
+import { useLocalStorage } from 'src/hooks/useLocalStorage';
+
 export const HookTest = (props) => {
-  const [storage, setStorage] = useLocalStorage('keep', 0);
+  const [storage, setStorage] = useLocalStorage('storage', 0);
   const handleClick = () => {
     setStorage((prev) => prev + 1);
   };
   return (
     <>
-      <div>{storage}</div>
+      <div>
+        <span style='padding:0.4rem'>{storage}</span>
+        <button onClick={handleClick}>click</button>
+      </div>
     </>
   );
 };
